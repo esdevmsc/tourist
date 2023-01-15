8f7dc1ad6ad454bf4a0ce15afbeb95cbef38ef13
fastasashark <warcraftoff@gmail.com>
2023-01-15 16:30:31 +1000

    Пишем динамическое заполнение таблицы с маршрутами

--- scripts.js
+++ scripts.js
@@ -4,10 +4,27 @@ function get_routes(){
   let url = "http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes";
   fetch(url + "?api_key=" + api_key)
   .then(response => response.json())
-  .then(data => {   
-      
+  .then(data => {      
       if(data.error === undefined){
-        console.log(data);
+        let tbody = document.getElementById('routes_tbody');
+        data.forEach(function(item, i, data) {          
+          let tr = document.createElement('tr');          
+          let td_name = document.createElement('td');          
+          td_name.innerHTML = item.name;
+          let td_description = document.createElement('td');
+          td_description.innerHTML = item.description;
+          let td_object = document.createElement('td');
+          td_object.innerHTML = item.mainObject;
+          let td_button = document.createElement('td');
+          td_button.innerHTML = '<button class="btn btn-outline-primary btn-sm">Выбрать</button>';
+          
+          tr.append(td_name);
+          tr.append(td_description);
+          tr.append(td_object);
+          tr.append(td_button);
+          tbody.append(tr);
+          //console.log(item);
+        });        
       }
       else{
         console.log(data.error);
